958210ea58772823519b23d97393ebf619bd3a1c
tim <timothee.ewart@epfl.ch>
2015-08-21 17:58:08 +0200

    estrin work in progress

--- poly/estrin.h
+++ poly/estrin.h
@@ -9,13 +9,8 @@
 #ifndef poly_estrin_h
 #define poly_estrin_h
 
-template<int n>
-constexpr int helper(){
-    return (n/poly_order::value > 1) ? true : false;
-};
-
 /* general test case, n is lower than the degree of the polynomial */
-template<int n, int q = n/poly_order::value, int r = n%poly_order::value>
+template<int n, bool b = (n<=poly_order::value)>
 struct binomial_helper{
     inline const static double binomial(double const& x){
         return coeff<n>::coefficient() + x*coeff<n+1>::coefficient(); // a+b*x
@@ -24,15 +19,15 @@ struct binomial_helper{
 
 /* perticular case, n the degree of the polynomial */
 template<>
-struct binomial_helper<poly_order::value,1,0>{
+struct binomial_helper<poly_order::value>{
     inline const static double binomial(double const& x){
         return coeff<poly_order::value>::coefficient(); // a
     }
 };
 
 /* requesting polynomial that does not exist */
-template<int n, int r>
-struct binomial_helper<n,1,r>{ // q should > 1 (2, 3 ....)
+template<int n>
+struct binomial_helper<n,false>{
     inline const static double binomial(double const& x){
         return 0;
     }
@@ -43,21 +38,22 @@ inline double quadratic(double const& x){
     return binomial_helper<n>::binomial(x) + pow<2>(x)*binomial_helper<n+2>::binomial(x); // (a+b*x)+(c+d*x)*x^2
 }
 
-/*
-
-
-template<int n>
+template<int n, bool = (n<=poly_order::value)>
 struct helper_estrin{
     static inline double estrin(double const& x){
-        return pow<2*n>(a)*... + pow<
+        return pow<2*n>(x)*quadratic<n>(x) + pow<2*n+1>(x)*helper_estrin<n+2>::estrin(x);
+    }
+};
+ 
+template<int n>
+struct helper_estrin<n,false>{
+    static inline double estrin(double const& x){
+        return 0;
     }
 };
 
-*/
-
 double estrin(double const& x){
-//    return binomial_helper<0>::binomial(x) + helper_estrin<poly_order::value>(x);
-    return quadratic<10>(x);
+    return binomial_helper<0>::binomial(x) + helper_estrin<4>::estrin(x);
 }
 
 #endif
